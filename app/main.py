4300e4fcc85a5526e1702c0c8dea037ddd55932e
Michal <mspytlos@gmail.com>
2023-12-30 18:14:16 +0100

    Manage db tables with alembic

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
-from . import database, models
 from .routers import auth, post, user
 
 app = FastAPI()
@@ -8,8 +7,6 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
-
-models.Base.metadata.create_all(bind=database.engine)
 
 
 @app.get("/")
