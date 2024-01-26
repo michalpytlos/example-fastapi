6e89b32b51d69c4d48d77739303280563a38658e
Michal <mspytlos@gmail.com>
2024-01-26 22:14:07 +0100

    Add script for running tests locally

---
+++ test.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Script args
+PYTEST_ARGS=""
+TESTS_TO_EXECUTE="tests"
+while getopts ":chf:" opt; do
+    case $opt in
+        c)
+            PYTEST_ARGS="--cov=app --cov-report=term-missing:skip-covered --cov-branch --cov-report=html"
+            ;;
+        f)  
+            TESTS_TO_EXECUTE="tests/${OPTARG}"
+            ;;
+        h)
+            echo "Usage: test.sh [-c] [-f <file>]"
+            echo "  -c: Run tests with coverage"
+            echo "  -f <file>: Run tests from given file only"
+            echo "  -h: Show this help message"
+            exit 0
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+    esac
+done
+
+# Cleanup if containers are started by this script
+top_output=$(docker compose top)
+
+cleanup() {
+    if [ -z "$top_output" ]; then 
+        docker compose down
+    fi
+}
+
+trap cleanup EXIT
+
+# Run db
+docker compose up -d postgres
+until pg_isready -h localhost -p 5432; do sleep 1; done;
+
+
+# Load env vars
+while read -r line; do
+    export "$(echo $line)"
+done < .env.local
+
+# Run tests
+COMMAND="pytest $PYTEST_ARGS -s -vv $TESTS_TO_EXECUTE"
+echo $COMMAND
+eval $COMMAND
